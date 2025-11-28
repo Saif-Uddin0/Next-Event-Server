81a9d4e107d62b6413c3fc787948c6f2415c7b7a
Saif Uddin <mahirr.846@gmail.com>
2025-11-28 14:18:21 +0600

    added the api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -37,18 +37,39 @@ async function run() {
     const db = client.db('events-db');
     const eventCollection = db.collection('events');
 
-    app.get('/events' , async(req, res)=>{
+    app.get('/events', async (req, res) => {
       const result = await eventCollection.find().toArray()
       res.send(result)
     })
 
+    app.get('/events/:id', async (req, res) => {
+      const { id } = req.params
+      const result = await eventCollection.findOne({ _id: new ObjectId(id) })
+      res.send(result);
+    })
+
+    // to add data to db
+    app.post('/events', async (req, res) => {
+      const data = req.body;
+      const result = await eventCollection.insertOne(data)
+      res.send({
+        success: true,
+        result
+      })
+    })
+    app.delete('/events/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await eventCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    
+
 
 
   }
